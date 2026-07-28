7e365af9a52e9fb47a8b262bb0f3b33a420b2065
Irfan Qureshi <irfaniq21@gmail.com>
2026-07-28 22:41:06 +0530

    Added Book Class with its Interface

---
+++ IBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreConsoleApp
+{
+    public interface IBook
+    {
+        bool AddBook(string title, string author, decimal price, int quantity);
+
+        bool RemoveBook(string title);
+
+
+        bool UpdateBook(string title, string author, decimal price, int quantity);
+
+    }
+}
